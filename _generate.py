65e3639625e7c730e8a0a6a79d0fc64f51757c1a
Tommy Keswick <t4k@users.noreply.github.com>
2022-09-12 15:07:58 -0700

    Update _generate.py

--- _generate.py
+++ _generate.py
@@ -6,20 +6,22 @@ import sys
 def main(file):
     print(f"🐞 file: {file}")
     # TODO think through handling of _closed files
-    if file.startswith("web/"):
+    if file.startswith("transcripts/"):
         # file_segments[1] will be component_id
         file_segments = file.split("/")
         if os.path.isfile(f"build/{file_segments[1]}/{file_segments[1]}.html"):
             print(f"🐞 file exists: build/{file_segments[1]}/{file_segments[1]}.html")
             return
-        os.mkdir(file_segments[1])
+        os.makedirs(f"build/{file_segments[1]}", exist_ok=True)
         subprocess.run(
             [
                 "pandoc",
+                "--standalone",
                 "--from=markdown",
                 "--to=html",
+                "--template=_html.tpl",
                 f"--output=build/{file_segments[1]}/{file_segments[1]}.html",
-                f"web/{file_segments[1]}/{file_segments[1]}.md",
+                f"transcripts/{file_segments[1]}/{file_segments[1]}.md",
             ]
         )
         print(f"🐞 file generated: build/{file_segments[1]}/{file_segments[1]}.html")
